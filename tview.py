1d7cd713fd09e4f41d33cdaf5bbd0071edd07ab9
Prem Agrawal <11preamagrawal@gmail.com>
2020-06-17 22:00:34 +0530

    Max Retry Error Solved but Element value not Changes

--- tview.py
+++ tview.py
@@ -4,14 +4,12 @@ import time
 import requests
 DRIVER = 'chromedriver'
 driver = webdriver.Chrome(DRIVER)
+driver.implicitly_wait(60)
 driver.get("https://www.tradingview.com/#signin")
-driver.find_element_by_name("username").send_keys("USERNAME")
-driver.find_element_by_name("password").send_keys("PASSWORD")
+driver.find_element_by_name("username").send_keys("username")
+driver.find_element_by_name("password").send_keys("password")
 driver.find_element_by_xpath("//button[@type='submit']").click()
-try :
-    driver.find_element_by_link_text('Chart').click()
-except :
-    driver.quit()
-#driver.find_element_by_class_name('input-3lfOzLDc').clear()
-#driver.find_element_by_class_name('input-3lfOzLDc').send_keys('SBIN')
+driver.find_element_by_link_text('Chart').click()
+driver.find_element_by_xpath('/html/body/div[2]/div[5]/div/div[1]/div[1]/div[1]/div[1]/div[1]/span/input').clear()
+driver.find_element_by_xpath('/html/body/div[2]/div[5]/div/div[1]/div[1]/div[1]/div[1]/div[1]/span/input').send_keys('SBIN')
 driver.quit()
