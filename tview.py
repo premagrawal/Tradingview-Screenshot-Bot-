ed4f591fa1f5d326a8ae303423f7733360931af8
Prem Agrawal <11preamagrawal@gmail.com>
2022-12-27 19:12:58 +0530

    Add files via upload

--- tview.py
+++ tview.py
@@ -1,63 +1,105 @@
-from Codes import *
 from selenium.webdriver.support import expected_conditions as EC
-from selenium.webdriver.common.keys import Keys
-from selenium import webdriver
-from selenium.webdriver.common.by import By
+from selenium.webdriver.common.action_chains import ActionChains
 from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.common.by import By
+from selenium import webdriver
+import pandas as pd
 import time
-import pandas
-try :
-    id = input('Enter your Login ID - ')
-    password = input('Enter your Password - ')
-    tf = input('Enter chart timeframe  - ')
-    DRIVER = 'chromedriver'
-    driver = webdriver.Chrome(DRIVER)
-    driver.maximize_window()
-    driver.get("https://www.tradingview.com/#signin")
-    driver.find_element_by_xpath('//*[@id="overlap-manager-root"]/div/div[2]/div/div/div/div/div/div[1]/div[6]/div/div[1]/div/span').click()
-    driver.find_element_by_name("username").send_keys(id)
-    driver.find_element_by_name("password").send_keys(password)
-    driver.find_element_by_xpath("//button[@type='submit']").click()
-    time.sleep(5)
-    driver.find_element_by_link_text('Chart').click()
-    time.sleep(5)
-except :
-    driver.quit()
-#This code changes Timeframe
-def change_tf(driver,tf):
-    tf_list = ['5m','15m','30m','1H','1D']
-    index = str('[' + str(int(tf_list.index(tf)) + 1 ) + ']')
-    driver.find_element_by_xpath('//*[@id="header-toolbar-intervals"]/div'+ index).click()
-#This function gets called in link_generator if symbol name is changed and generates a URL
-def take_screenshot(driver):
-    time.sleep(5)
-    driver.find_element_by_id("header-toolbar-screenshot").click()
-    WebDriverWait(driver,10).until(EC.visibility_of_element_located((By.CLASS_NAME,"textInput-3WRWEmm7")))
-    link = driver.find_element_by_class_name("textInput-3WRWEmm7").get_attribute("value")
-    WebDriverWait(driver,5).until(EC.visibility_of_element_located((By.CLASS_NAME,"close-3kPn4OTV")))
-    driver.find_element_by_class_name("close-3kPn4OTV").click()
-    return link
 
-#This function clears name of the previous symbols and types new symbol name
-def link_generator(driver, stock_name):
-    e = driver.find_element_by_class_name('input-3lfOzLDc')
-    e.click()
-    e.send_keys(Keys.CLEAR)
-    e.send_keys(stock_name)
-    e.send_keys(Keys.RETURN)
-    if e.get_attribute('value') == stock_name:
-        links = (take_screenshot(driver))
-        return links
-try :
-    value = heatmap_list()
-    url_list = []
-    for v in value:
-        change_tf(driver,tf)
-        link = link_generator(driver,v)
-        print(v,'-',link)
-        url_list.append(link)
-    df = pandas.DataFrame({'Company Name' : value,
-                            'Link'        : url_list})
-    df.to_csv('sample.csv',header=True)
-finally :
-    driver.quit()
+csv_path = "/Users/premagrawal/Dropbox/My Mac (Prems-MacBook-Air.local)/Desktop/Entropy/doper.csv"
+df = pd.read_csv(csv_path)
+del df["Unnamed: 0"]
+df["Date"] = pd.to_datetime(df["Date"]).dt.date
+df = (df[df['Risk Reward'] > 2 ]).sort_values(by='Risk Reward',ascending=False)
+urlList    = []
+driver     = webdriver.Chrome('chromedriver')
+driver.maximize_window()
+driver.get("https://chrome.google.com/webstore/detail/adblock-%E2%80%94-best-ad-blocker/gighmmpiobklfepjocnamgkkbiglidom")
+time.sleep(10)
+driver.get('https://www.tradingview.com/#signin')
+driver.implicitly_wait(60)
+time.sleep(1)
+driver.find_element(By.XPATH,'//*[@id="overlap-manager-root"]/div/div[2]/div/div/div/div/div/div/div[1]/div[4]/div/span').click()
+username = '11preamagrawal@gmail.com'
+password = 'Mustang@4785'
+driver.implicitly_wait(60)
+time.sleep(2)
+driver.find_element(By.NAME,"username").send_keys(username)
+driver.find_element(By.NAME,"password").send_keys(password)
+driver.find_element(By.XPATH,"//button[@type='submit']").click()
+driver.implicitly_wait(60)
+time.sleep(5)
+driver.find_element(By.LINK_TEXT,'Chart').click()
+driver.implicitly_wait(60)
+time.sleep(5)
+for index in range(45,len(df)):
+    nextChart = input("Do you want to look at next chart : ")
+    if nextChart == "Y" or nextChart == "y":
+        print(f"Opening {index}th Chart")
+        try:
+            driver.find_element(By.CLASS_NAME,"chart-gui-wrapper").click()
+            driver.find_element(By.XPATH,'//*[@id="header-toolbar-symbol-search"]').click()
+        except:
+            driver.find_element(By.XPATH,'//*[@id="header-toolbar-symbol-search"]/div').click()
+        driver.implicitly_wait(60)
+        stocks = "NSE:" + df.iloc[index]["Stocks"]
+        date   = df.iloc[index]["Date"]
+        driver.find_element(By.XPATH,'//*[@id="overlap-manager-root"]/div/div/div[2]/div/div[2]/div[1]/input').send_keys(stocks)
+        driver.implicitly_wait(60)
+        driver.find_element(By.XPATH,'//*[@id="overlap-manager-root"]/div/div/div[2]/div/div[2]/div[1]/input').send_keys(Keys.ENTER)
+        driver.implicitly_wait(60)
+        """
+        #Close Ad
+        try:
+            driver.find_element(By.CLASS_NAME,"close-icon-r9bFT7mJ").click()
+            driver.implicitly_wait(60)
+            try:
+                driver.find_element(By.CLASS_NAME,"close-button-aR0iEGbS closeButton-GLTtix84").click()
+            except:
+                try:
+                    driver.find_element(By.XPATH,'//*[@id="overlap-manager-root"]/div[2]/div/div[1]/button').click()
+                except:
+                    pass
+        except:
+            pass
+        """
+        #Jump to Date
+        time.sleep(4)
+        try:
+            driver.find_element(By.CLASS_NAME,'icon-rgQ5VWWu').click()
+        except:
+            driver.find_element(By.CLASS_NAME,"close-icon-r9bFT7mJ").click()
+            time.sleep(1)
+            try:
+                driver.find_element(By.CLASS_NAME,"close-button-aR0iEGbS closeButton-GLTtix84").click()
+            except:
+                try:
+                    driver.find_element(By.XPATH,'//*[@id="overlap-manager-root"]/div[2]/div/div[1]/button').click()
+                except:
+                    pass
+        driver.implicitly_wait(60)
+        element = driver.find_element(By.XPATH,'//*[@id="overlap-manager-root"]/div/div/div[1]/div/div[3]/div/div/div[1]/div[1]/div/div/div/span/span[1]/input')
+        for items in range(10):
+            element.send_keys(Keys.BACKSPACE)
+        driver.implicitly_wait(60)
+        element.send_keys(str(date))
+        driver.implicitly_wait(60)
+        driver.find_element(By.NAME,"submit").click()
+        time.sleep(1)
+        #Link-Generate
+        driver.find_element(By.XPATH,'//*[@id="header-toolbar-screenshot"]').click()
+        driver.implicitly_wait(60)
+        driver.find_element(By.XPATH,'//*[@id="overlap-manager-root"]/div/span/div[1]/div/div/div[4]/span[2]/span/div/span[2]/span').click()
+        time.sleep(3)
+        parent = driver.window_handles[0]
+        child  = driver.window_handles[1]
+        driver.switch_to.window(child)
+        url = (driver.current_url)
+        if "blob" not in url:
+            urlList.append(url)
+        else:
+            time.sleep(5)
+            urlList.append(url)
+        driver.close()
+        driver.switch_to.window(parent)
